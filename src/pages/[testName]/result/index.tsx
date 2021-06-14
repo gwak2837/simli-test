3e785954f7c2d16208b2ab9f88265de324149501
Taeuk Gwak <gwak2837@kakao.com>
2021-06-14 16:16:46 +0900

    Create TestResultPage

---
+++ src/pages/[testName]/result/index.tsx
@@ -0,0 +1,13 @@
+import PageHead from 'src/components/layouts/PageHead'
+
+const description = '결과를 확인해 보아요'
+
+function TestResultPage() {
+  return (
+    <PageHead title="심리테스트 - 결과" description={description}>
+      {}
+    </PageHead>
+  )
+}
+
+export default TestResultPage
