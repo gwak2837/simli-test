ffb56a82249de3ea79b7350099dbda29758b2d9f
Taeuk Gwak <gwak2837@kakao.com>
2021-06-14 17:53:08 +0900

    Rename files

--- src/pages/tests/[name]/index.tsx
+++ src/pages/tests/[name]/index.tsx
@@ -1,10 +1,13 @@
+import { useRouter } from 'next/router'
 import PageHead from 'src/components/layouts/PageHead'
 
 const description = '심리 테스트를 진행해보아요'
 
 function TestPage() {
+  const { query } = useRouter()
+
   return (
-    <PageHead title="심리테스트 - " description={description}>
+    <PageHead title={`심리테스트 - ${query.name}`} description={description}>
       {}
     </PageHead>
   )
