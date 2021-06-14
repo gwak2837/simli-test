a6ef94ebf0fbf1b755b1d252fbbaf61b53c34b9d
Taeuk Gwak <gwak2837@kakao.com>
2021-06-14 17:53:20 +0900

    Format codes

--- src/components/layouts/NavigationLayout.tsx
+++ src/components/layouts/NavigationLayout.tsx
@@ -6,6 +6,7 @@ import Header from '../Header'
 const BackgroundColor = styled.div`
   background-color: #fcfcfc !important;
 `
+
 export const Padding = styled.div`
   padding: ${HEADER_HEIGHT} 0 0;
 `
