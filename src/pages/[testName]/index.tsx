b6aac75c2187d4eb25deb8927b962f63e48c794d
Taeuk Gwak <gwak2837@kakao.com>
2021-06-14 16:16:52 +0900

    Create TestPage

---
+++ src/pages/[testName]/index.tsx
@@ -0,0 +1,13 @@
+import PageHead from 'src/components/layouts/PageHead'
+
+const description = '심리 테스트를 진행해보아요'
+
+function TestPage() {
+  return (
+    <PageHead title="심리테스트 - " description={description}>
+      {}
+    </PageHead>
+  )
+}
+
+export default TestPage
