a73aa354f3d0b9d1f4867768c44ca26054272983
Taeuk Gwak <gwak2837@kakao.com>
2021-06-15 12:32:55 +0900

    Create question-result.ts

---
+++ src/models/question-result.ts
@@ -0,0 +1,9 @@
+export const questions: Record<string, any[]> = {
+  Test_1: ['Q. 당신은 점심 먹었나요?', 'Q. 당신은 저녁 먹었나요?'],
+  Test_2: ['질문1', '질문2'],
+}
+
+export const results = {
+  testName: '결과',
+  testName2: '결과',
+}
