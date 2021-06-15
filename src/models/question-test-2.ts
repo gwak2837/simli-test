7e4738d5121fc2fd231d09c69040d9aa33b1e9c1
Taeuk Gwak <gwak2837@kakao.com>
2021-06-15 15:00:18 +0900

    Delete question-test-2.ts

--- src/models/question-test-2.ts
+++
@@ -1,7 +0,0 @@
-export const TEST2_QUESTION1 = '나는 조선의 선비다.'
-export const TEST2_QUESTION2 =
-  '위층에 새로운 노비가 이사를 왔다. 그는 조선의 덕을 알지 못한다. 그와 이웃이 될 것인가?'
-export const TEST2_QUESTION3 = '나는 배가 고플 때 의식을 셧다운 한다.'
-export const TEST2_QUESTION4 =
-  '맞춤법 따위는 개나 줘버려. 나는 킹 세종의 뜻을 헤아리기엔 덕이 부족하다.'
-export const TEST2_QUESTION5 = '좀비는 인간과 친구가 될 수 있다.'
