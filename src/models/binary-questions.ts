5a7eef21e98f2f43d6bfe5f51183f5379fbc59f2
gundopark-123 <85215022+gundopark-123@users.noreply.github.com>
2021-06-15 16:45:16 +0900

    binary-questions added
    more questions are added

--- src/models/binary-questions.ts
+++ src/models/binary-questions.ts
@@ -25,24 +25,24 @@ export const tests: Record<string, Test> = {
         id: '1',
         question: 'Q. 당신은 점심 먹었나요?',
         onYes: [
-          { id: '1', name: '충족감', value: 1 },
-          { id: '2', name: '허기짐', value: -1 },
+          { id: '1', name: '평온', value: 10 },
+          { id: '2', name: '짜증', value: -10 },
         ],
         onNo: [
-          { id: '1', name: '심리적 안정감', value: -5 },
-          { id: '2', name: '배고픔', value: 10 },
+          { id: '1', name: '평온', value: -10 },
+          { id: '2', name: '짜증', value: 10 },
         ],
       },
       {
         id: '2',
         question: 'Q. 당신은 저녁 먹었나요?',
         onYes: [
-          { id: '1', name: 'a', value: 10 },
-          { id: '2', name: 'b', value: -10 },
+          { id: '1', name: '평온', value: 10 },
+          { id: '2', name: '짜증', value: -10 },
         ],
         onNo: [
-          { id: '1', name: 'a', value: -50 },
-          { id: '2', name: 'b', value: 100 },
+          { id: '1', name: '평온', value: -10 },
+          { id: '2', name: '짜증', value: 10 },
         ],
       },
     ],
@@ -55,12 +55,13 @@ export const tests: Record<string, Test> = {
         id: '1',
         question: 'Q. 나는 조선의 선비다.',
         onYes: [
-          { id: '1', name: '선비 정신', value: 1 },
-          { id: '2', name: '힙 정신', value: -1 },
+          { id: '1', name: '선비 정신', value: 10 },
+          { id: '2', name: '힙 정신', value: -10 },
+          { id: '3', name: '평온', value: 10 },
         ],
         onNo: [
-          { id: '1', name: 'a', value: -5 },
-          { id: '2', name: 'b', value: 10 },
+          { id: '1', name: '선비 정신', value: -5 },
+          { id: '2', name: '힙 정신', value: 10 },
         ],
       },
       {
@@ -68,48 +69,113 @@ export const tests: Record<string, Test> = {
         question:
           'Q. 위층에 새로운 노비가 이사를 왔다. 그는 조선의 덕을 알지 못한다. 그와 이웃이 될 것인가?',
         onYes: [
-          { id: '1', name: 'a', value: 10 },
-          { id: '2', name: 'b', value: -10 },
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
         ],
         onNo: [
-          { id: '1', name: 'a', value: -50 },
-          { id: '2', name: 'b', value: 100 },
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+          { id: '3', name: '짜증', value: 10 },
         ],
       },
       {
         id: '3',
         question: 'Q. 나는 배가 고플 때 의식을 셧다운 한다.',
         onYes: [
-          { id: '1', name: 'a', value: 10 },
-          { id: '2', name: 'b', value: -10 },
+          { id: '1', name: '평온', value: 10 },
+          { id: '2', name: '짜증', value: -10 },
         ],
         onNo: [
-          { id: '1', name: 'a', value: -50 },
-          { id: '2', name: 'b', value: 100 },
+          { id: '1', name: '평온', value: -10 },
+          { id: '2', name: '짜증', value: 10 },
         ],
       },
       {
         id: '4',
         question: 'Q. 맞춤법 따위는 개나 줘버려. 나는 킹 세종의 뜻을 헤아리기엔 덕이 부족하다.',
         onYes: [
-          { id: '1', name: 'a', value: 10 },
-          { id: '2', name: 'b', value: -10 },
+          { id: '1', name: '평온', value: -10 },
+          { id: '2', name: '짜증', value: 10 },
+          { id: '3', name: '힙 정신', value: 10 },
         ],
         onNo: [
-          { id: '1', name: 'a', value: -50 },
-          { id: '2', name: 'b', value: 100 },
+          { id: '1', name: '평온', value: 10 },
+          { id: '2', name: '짜증', value: -10 },
         ],
       },
       {
         id: '5',
         question: 'Q. 좀비는 인간과 친구가 될 수 있다.',
         onYes: [
-          { id: '1', name: 'a', value: 10 },
-          { id: '2', name: 'b', value: -10 },
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
         ],
         onNo: [
-          { id: '1', name: 'a', value: -50 },
-          { id: '2', name: 'b', value: 100 },
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+        ],
+      },
+      {
+        id: '6',
+        question: 'Q. 나는 외제차를 타는 미제 앞잡이들과 상종할 수 없다.',
+        onYes: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+        ],
+        onNo: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+        ],
+      },
+      {
+        id: '7',
+        question: 'Q. 위대하신 령도자! 우리 민족끼리 단합하자!',
+        onYes: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+        ],
+        onNo: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+        ],
+      },
+      {
+        id: '8',
+        question: 'Q. 배고플때 먹빵을 보며 대리만족을 한다.',
+        onYes: [
+          { id: '1', name: '평온', value: 10 },
+          { id: '2', name: '짜증', value: -10 },
+          { id: '3', name: '선비 정신', value: 20 },
+        ],
+        onNo: [{ id: '2', name: '힙 정신', value: -10 }],
+      },
+      {
+        id: '9',
+        question: 'Q. 배고플때 먹빵을 보며 배달의 민족 콜.',
+        onYes: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+          { id: '3', name: '평온', value: 10 },
+          { id: '4', name: '짜증', value: -10 },
+        ],
+        onNo: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
+          { id: '3', name: '평온', value: -10 },
+          { id: '4', name: '짜증', value: 10 },
+        ],
+      },
+      {
+        id: '10',
+        question:
+          'Q. 중고물품을 거래하러 교대역 1번 출구로 나왔다. 저기 수상쩍은 남자가 당신에게 "당근이세요?"라고 물어볼 때 당신은 "오이인데요."라고 대답한다.',
+        onYes: [
+          { id: '1', name: '선비 정신', value: 10 },
+          { id: '2', name: '힙 정신', value: -20 },
+        ],
+        onNo: [
+          { id: '1', name: '선비 정신', value: -10 },
+          { id: '2', name: '힙 정신', value: 10 },
         ],
       },
     ],
