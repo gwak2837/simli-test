79171f9ff66f401da50a428dbd22d274947aae92
Taeuk Gwak <gwak2837@kakao.com>
2021-06-15 12:33:12 +0900

    Add answer click listener

--- src/pages/tests/[name]/index.tsx
+++ src/pages/tests/[name]/index.tsx
@@ -1,7 +1,8 @@
 import { useRouter } from 'next/router'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import BinaryQuestionAnswer from 'src/components/BinaryQuestionAnswer'
 import PageHead from 'src/components/layouts/PageHead'
+import { questions } from 'src/models/question-result'
 import { GlobalContext } from 'src/pages/_app'
 
 const description = '심리 테스트를 진행해보아요'
@@ -9,15 +10,37 @@ const description = '심리 테스트를 진행해보아요'
 function TestPage() {
   const { data, data2 } = useContext(GlobalContext)
 
-  const { query } = useRouter()
+  const [questionNumber, setQuestionNumber] = useState(0)
+
+  const router = useRouter()
+
+  const [questionName, setQuestionName] = useState('')
+
+  useEffect(() => {
+    setQuestionName((router.query.name ?? '') as string)
+  }, [router.query.name])
+
+  const question = questions[questionName] ? questions[questionName][questionNumber] : ''
 
   return (
-    <PageHead title={`심리테스트 - ${query.name}`} description={description}>
-      <h2>{query.name}</h2>
+    <PageHead title={`심리테스트 - ${router.query.name ?? ''}`} description={description}>
+      <h2>{router.query.name}</h2>
       <BinaryQuestionAnswer
-        question="Q. 당신은 점심 먹었나요?"
-        onYes={() => console.log('네')}
-        onNo={() => console.log('아니요')}
+        question={question}
+        onYes={() => {
+          if (questionNumber >= questions[questionName].length - 1) {
+            router.push(`/tests/${questionName}/result`)
+          } else {
+            setQuestionNumber((prev) => prev + 1)
+          }
+        }}
+        onNo={() => {
+          if (questionNumber >= questions[questionName].length - 1) {
+            router.push(`/tests/${questionName}/result`)
+          } else {
+            setQuestionNumber((prev) => prev + 1)
+          }
+        }}
       />
     </PageHead>
   )
