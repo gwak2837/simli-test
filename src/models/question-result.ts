ce2659d8785229045d4b3dcb0994e2676bfe66a4
gundopark-123 <85215022+gundopark-123@users.noreply.github.com>
2021-06-15 14:45:10 +0900

    TEST_2_QUESTIONS ADDED

--- src/models/question-result.ts
+++ src/models/question-result.ts
@@ -1,10 +1,23 @@
+import {
+  TEST2_QUESTION1,
+  TEST2_QUESTION2,
+  TEST2_QUESTION3,
+  TEST2_QUESTION4,
+  TEST2_QUESTION5,
+} from 'src/models/question-test-2'
 export const tests: Record<string, Record<string, string[]>> = {
   Test_1: {
     questions: ['Q. 당신은 점심 먹었나요?', 'Q. 당신은 저녁 먹었나요?'],
     results: ['결과1', '결과2'],
   },
   Test_2: {
-    questions: ['Q. 당신은 점심 먹었나요?', 'Q. 당신은 저녁 먹었나요?'],
+    questions: [
+      TEST2_QUESTION1,
+      TEST2_QUESTION2,
+      TEST2_QUESTION3,
+      TEST2_QUESTION4,
+      TEST2_QUESTION5,
+    ],
     results: ['결과1', '결과2'],
   },
   Test_3: {
