937e25ba0b1d8649afa5ab5673b4657a5dc3e7da
Taeuk Gwak <gwak2837@kakao.com>
2021-06-15 23:18:00 +0900

    Update packages

--- src/pages/tests/index.tsx
+++ src/pages/tests/index.tsx
@@ -2,7 +2,6 @@ import PageHead from 'src/components/layouts/PageHead'
 import styled from 'styled-components'
 import Image from 'next/image'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
-import { SquareFrame } from '..'
 import { fadeIn } from 'src/styles/SpecialEffect'
 
 const RelativePosition = styled.div`
@@ -18,7 +17,7 @@ const RelativePosition = styled.div`
   }
 
   :hover {
-    > text {
+    > h3 {
       animation-name: ${fadeIn};
       animation-duration: 1s;
       opacity: 1;
@@ -31,7 +30,7 @@ const RelativePosition = styled.div`
   }
 `
 
-const TestExplainText = styled.text`
+const TestExplainText = styled.h3`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -40,17 +39,13 @@ const TestExplainText = styled.text`
   font-size: 1.4rem;
 `
 
-const TestSelectGridContainer = styled.nav`
+const GridContainer = styled.nav`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr;
-  margin: 0;
-  padding: 0;
-  height: 100%;
+
   width: 100%;
-  top: 0;
-  left: 0;
-  position: absolute;
+  aspect-ratio: 1 / 1;
   background: #eee;
 `
 
@@ -60,34 +55,32 @@ function TestsPage() {
   return (
     <PageHead title="심리테스트 - 테스트 목록" description={description}>
       <h1>원하는 테스트를 골라보세요</h1>
-      <SquareFrame>
-        <TestSelectGridContainer>
-          <RelativePosition>
-            <TestExplainText>TV중독TEST</TestExplainText>
-            <ClientSideLink href="/tests/Test_1">
-              <Image src="/TV_addict.jpg" alt="test-1-image" layout="fill" objectFit="cover" />
-            </ClientSideLink>
-          </RelativePosition>
-          <RelativePosition>
-            <TestExplainText>스마트폰중독TEST</TestExplainText>
-            <ClientSideLink href="/tests/Test_2">
-              <Image src="/Phone_addict.jpg" alt="test-1-image" layout="fill" objectFit="cover" />
-            </ClientSideLink>
-          </RelativePosition>
-          <RelativePosition>
-            <TestExplainText>성격TEST</TestExplainText>
-            <ClientSideLink href="/tests/Test_3">
-              <Image src="/Personality.jpg" alt="test-image" layout="fill" objectFit="cover" />
-            </ClientSideLink>
-          </RelativePosition>
-          <RelativePosition>
-            <TestExplainText>성격에 맞는직업TEST</TestExplainText>
-            <ClientSideLink href="/tests/Test_4">
-              <Image src="/Personality_JOB.jpg" alt="test-image" layout="fill" objectFit="cover" />
-            </ClientSideLink>
-          </RelativePosition>
-        </TestSelectGridContainer>
-      </SquareFrame>
+      <GridContainer>
+        <RelativePosition>
+          <TestExplainText>TV중독TEST</TestExplainText>
+          <ClientSideLink href="/tests/Test_1">
+            <Image src="/TV_addict.jpg" alt="test-1-image" layout="fill" objectFit="cover" />
+          </ClientSideLink>
+        </RelativePosition>
+        <RelativePosition>
+          <TestExplainText>스마트폰중독TEST</TestExplainText>
+          <ClientSideLink href="/tests/Test_2">
+            <Image src="/Phone_addict.jpg" alt="test-1-image" layout="fill" objectFit="cover" />
+          </ClientSideLink>
+        </RelativePosition>
+        <RelativePosition>
+          <TestExplainText>성격TEST</TestExplainText>
+          <ClientSideLink href="/tests/Test_3">
+            <Image src="/Personality.jpg" alt="test-image" layout="fill" objectFit="cover" />
+          </ClientSideLink>
+        </RelativePosition>
+        <RelativePosition>
+          <TestExplainText>성격에 맞는직업TEST</TestExplainText>
+          <ClientSideLink href="/tests/Test_4">
+            <Image src="/Personality_JOB.jpg" alt="test-image" layout="fill" objectFit="cover" />
+          </ClientSideLink>
+        </RelativePosition>
+      </GridContainer>
     </PageHead>
   )
 }
