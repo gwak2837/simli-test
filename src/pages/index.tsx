6ba59c54e8a4fa3acade26b7bd2ee18f67552c39
birdhk71 <birdhk71@gmail.com>
2021-06-15 15:56:27 +0900

    modify useless code
    잘못 푸쉬해서 수정된 코드 제가 만든 테스트 페이지  돌려보느라 수정했습니다

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -52,31 +52,22 @@ const StartButton = styled(SecondaryButton)`
 
 const FlexContainerColumnCenter = styled(FlexContainerAlignCenter)`
   flex-flow: column nowrap;
-  padding: 10rem 0;
+  margin: 1rem 0;
 `
 
 const StartEffectTag = styled.div`
   animation: ${FocusInExpandFwd} 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
   text-align: center;
 `
-
-const TestSelectButton = styled(PrimaryButton)``
-
 function HomePage() {
-  const { data, data2 } = useContext(GlobalContext)
-
-  const goToTestFisrtTest = useGoToPage('/tests/Test_1')
-  const goToTestSecondTest = useGoToPage('/tests/Test_2')
-  const goToTestThirdTest = useGoToPage('/tests/Test_3')
-  const goToTestFourthTest = useGoToPage('/tests/Test_4')
-
+  const goToTestsPage = useGoToPage('/tests')
   return (
     <PageHead>
       <FlexContainerColumnCenter>
-        <ClientSideLink href="/users/username/regulars">
-          <StartImg src="/simli_main.png" />
+        <ClientSideLink href="/tests">
+          <StartImg src="/simli_main.jpg" />
         </ClientSideLink>
-        <StartButton>심리테스트 시작</StartButton>
+        <StartButton onClick={goToTestsPage}>심리테스트 시작</StartButton>
         <FlexContainerAlignCenter>
           <LocationText>
             <StartEffectTag>쟤도 날 좋아할까? 그/그녀의 심리를 알고 싶다면?</StartEffectTag>
@@ -88,12 +79,6 @@ function HomePage() {
             </LocationText>
             지금 당장 시작하세요.
           </LocationText>
-          <div>
-            <TestSelectButton onClick={goToTestFisrtTest}>심리Test1</TestSelectButton>
-            <TestSelectButton onClick={goToTestSecondTest}>심리Test2</TestSelectButton>
-            <TestSelectButton onClick={goToTestThirdTest}>심리Test3</TestSelectButton>
-            <TestSelectButton onClick={goToTestFourthTest}>심리Test4</TestSelectButton>
-          </div>
         </FlexContainerAlignCenter>
       </FlexContainerColumnCenter>
     </PageHead>
