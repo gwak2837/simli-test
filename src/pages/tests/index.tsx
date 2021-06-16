ade7832b8c5250c7d0ecdc6e1af454f4adbbc437
Taeuk Gwak <gwak2837@kakao.com>
2021-06-16 16:22:34 +0900

    Modify test list image to square

--- src/pages/tests/index.tsx
+++ src/pages/tests/index.tsx
@@ -3,9 +3,12 @@ import styled from 'styled-components'
 import Image from 'next/image'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
 import { fadeIn } from 'src/styles/SpecialEffect'
+import React from 'react'
+import { CenterPaddingH1 } from './[name]/result'
 
 const RelativePosition = styled.div`
   position: relative;
+  line-height: 0;
 
   @keyframes fadeOut1 {
     from {
@@ -17,7 +20,7 @@ const RelativePosition = styled.div`
   }
 
   :hover {
-    > h3 {
+    > h2 {
       animation-name: ${fadeIn};
       animation-duration: 1s;
       opacity: 1;
@@ -30,13 +33,15 @@ const RelativePosition = styled.div`
   }
 `
 
-const TestExplainText = styled.h3`
+const AbsolutePositionH2 = styled.h2`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
   opacity: 0;
-  font-size: 1.4rem;
+  text-align: center;
+  line-height: normal;
 `
 
 const GridContainer = styled.nav`
@@ -45,8 +50,8 @@ const GridContainer = styled.nav`
   grid-template-rows: 1fr 1fr;
 
   width: 100%;
-  height: 100vh; // 모든 브라우저에서 aspect-ratio 가 지원될 때까지 넣어 놓기
-  aspect-ratio: 1 / 1;
+  /* height: 100vh; // 모든 브라우저에서 aspect-ratio 가 지원될 때까지 넣어 놓기 */
+  /* aspect-ratio: 1 / 1; */
   background: #eee;
 `
 
@@ -55,30 +60,42 @@ const description = '테스트 목록을 확인해보세요'
 function TestsPage() {
   return (
     <PageHead title="심리테스트 - 테스트 목록" description={description}>
-      <h1>원하는 테스트를 골라보세요</h1>
+      <CenterPaddingH1>원하는 테스트를 골라보세요</CenterPaddingH1>
       <GridContainer>
         <RelativePosition>
-          <TestExplainText>일중독 TEST</TestExplainText>
+          <AbsolutePositionH2>일중독 TEST</AbsolutePositionH2>
           <ClientSideLink href="/tests/Test_1">
-            <Image src="/workaholic.jpg" alt="test-image" layout="fill" objectFit="cover" />
+            <Image
+              src="/workaholic.jpg"
+              alt="/workaholic.jpg"
+              width="300"
+              height="300"
+              objectFit="cover"
+            />
           </ClientSideLink>
         </RelativePosition>
         <RelativePosition>
-          <TestExplainText>조선시대 당신의 신분은?</TestExplainText>
+          <AbsolutePositionH2>조선시대 당신의 신분은?</AbsolutePositionH2>
           <ClientSideLink href="/tests/Test_2">
-            <Image src="/joseon.jpg" alt="test-image" layout="fill" objectFit="cover" />
+            <Image src="/joseon.jpg" alt="/joseon.jpg" width="300" height="300" objectFit="cover" />
           </ClientSideLink>
         </RelativePosition>
         <RelativePosition>
-          <TestExplainText>호구 TEST</TestExplainText>
+          <AbsolutePositionH2>호구 TEST</AbsolutePositionH2>
           <ClientSideLink href="/tests/Test_3">
-            <Image src="/hogu.jpg" alt="test-image" layout="fill" objectFit="cover" />
+            <Image src="/hogu.jpg" alt="/hogu.jpg" width="300" height="300" objectFit="cover" />
           </ClientSideLink>
         </RelativePosition>
         <RelativePosition>
-          <TestExplainText>자아도취 TEST</TestExplainText>
+          <AbsolutePositionH2>자아도취 TEST</AbsolutePositionH2>
           <ClientSideLink href="/tests/Test_4">
-            <Image src="/narcissism.jpg" alt="test-image" layout="fill" objectFit="cover" />
+            <Image
+              src="/narcissism.jpg"
+              alt="test-image"
+              width="300"
+              height="300"
+              objectFit="cover"
+            />
           </ClientSideLink>
         </RelativePosition>
       </GridContainer>
