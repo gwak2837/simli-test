cebef6d604c773b5680947c926c31d6a025f138a
Taeuk Gwak <gwak2837@kakao.com>
2021-07-01 13:38:44 +0900

    Update login.tsx
    React hook form 적용

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,26 @@
+import { Input } from 'antd'
 import styled from 'styled-components'
 import useGoToPage from 'src/hooks/useGoToPage'
 import { PrimaryButton } from 'src/components/atoms/Button'
 import ClientSideLink from 'src/components/atoms/ClientSideLink'
+import { Controller, useForm } from 'react-hook-form'
 
 const Container = styled.div`
   margin-top: 100px;
-  padding: 20px;
+  padding: 1rem;
+`
+
+const GridContainerForm = styled.form`
+  display: grid;
+  gap: 1rem;
+
+  a {
+    text-align: center;
+  }
+`
+
+const PrimaryButtonWidth100 = styled(PrimaryButton)`
+  width: 100%;
 `
 
 const LogIn = styled.h3`
@@ -15,28 +30,54 @@ const LogIn = styled.h3`
   line-height: 1;
 `
 
-const Input = styled.input`
-  position: relative;
-  overflow: hidden;
-  width: 100%;
-  height: 40px;
-  margin: 0 0 8px;
-  padding: 5px 39px 5px 11px;
-  border: solid 1px #dadada;
-  background: #fff;
-  box-sizing: border-box;
-`
+type LoginForm = {
+  email: string
+  password: string
+}
 
 function LoginPage() {
-  const goToLoginPage = useGoToPage('/')
+  const { control, handleSubmit } = useForm<LoginForm>({
+    defaultValues: { email: '', password: '' },
+  })
+
+  function login({ email, password }: LoginForm) {
+    console.log('login', email, password)
+  }
 
   return (
     <Container>
-      <LogIn>LOGIN1</LogIn>
-      <Input id="id" name="id" placeholder="아이디를 입력해주세요" />
-      <Input id="password" name="password" type="password" placeholder="비밀번호를 입력해주세요" />
-      <PrimaryButton onClick={goToLoginPage}>로그인</PrimaryButton>
-      <ClientSideLink href="/register">회원가입</ClientSideLink>
+      <LogIn>LOGIN</LogIn>
+      <GridContainerForm onSubmit={handleSubmit(login)}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field }) => (
+              <Input size="large" type="email" placeholder="아이디를 입력해주세요" {...field} />
+            )}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Password</label>
+          <Controller
+            control={control}
+            name="password"
+            render={({ field }) => (
+              <Input
+                size="large"
+                type="password"
+                placeholder="비밀번호를 입력해주세요"
+                {...field}
+              />
+            )}
+          />
+        </div>
+
+        <PrimaryButtonWidth100 htmlType="submit">로그인</PrimaryButtonWidth100>
+        <ClientSideLink href="/register">회원가입</ClientSideLink>
+      </GridContainerForm>
     </Container>
   )
 }
